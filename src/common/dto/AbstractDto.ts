bfc4ecad7c9e705e99879a633388dc43cf3db474
0xb4lamx <medghaith.sellami@gmail.com>
2019-09-19 18:12:31 +0100

    [fix] AbstractDto.ts

--- src/common/dto/AbstractDto.ts
+++ src/common/dto/AbstractDto.ts
@@ -12,9 +12,9 @@ export class AbstractDto {
     updatedAt: Date;
 
     constructor(entity: AbstractEntity) {
-        this.id = entity.id;
-        this.createdAt = entity.createdAt;
-        this.updatedAt = entity.updatedAt;
+        // this.id = entity.id;
+        // this.createdAt = entity.createdAt;
+        // this.updatedAt = entity.updatedAt;
     }
 
 }
