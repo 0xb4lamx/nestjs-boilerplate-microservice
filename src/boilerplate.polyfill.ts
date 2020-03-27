d6ea28ff8a2357f03c5a001da7a49cdaede7fc85
0xb4lamx <medghaith.sellami@gmail.com>
2020-03-27 18:08:34 +0100

    chore: remove deprecated comments

--- src/boilerplate.polyfill.ts
+++ src/boilerplate.polyfill.ts
@@ -6,13 +6,11 @@ import { AbstractEntity } from './common/abstract.entity';
 import { AbstractDto } from './common/dto/abstract.dto';
 
 declare global {
-// tslint:disable-next-line:naming-convention no-unused
     interface Array<T> {
         toDtos<B extends AbstractDto>(this: AbstractEntity[]): B[];
     }
 }
 
 Array.prototype.toDtos = function<B extends AbstractDto>(): B[] {
-    // tslint:disable-next-line:no-invalid-this
     return _(this).map((item) => item.toDto()).compact().value() as B[];
 };
